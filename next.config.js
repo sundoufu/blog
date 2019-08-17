a9513282594c453c661ddf458ba92d1522501d3a
Stein Kim <wotjd1102@gmail.com>
2019-08-17 11:58:40 +0900

    nextjs 세팅 외
    * airbnb eslint 및 커스터마이즈
    * storybook 세팅
    * 페이지 및 컴포넌트의 확장자 jsx로 변경
    * 파일 네이밍 컨벤션 변경(컴포넌트 camelCase -> PascalCase)

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // eslint options (if necessary)
+        },
+      });
+    }
+    return {
+      ...config,
+      node: {
+        fs: 'empty',
+      },
+    };
+  },
+  target: 'serverless',
+};
