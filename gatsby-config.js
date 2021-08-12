9bee5cdb8dcd59b89185ea2b37776d7cdf409856
김재성 (Kim, Jaesung) <15705385+sorrykim@users.noreply.github.com>
2021-08-12 19:42:44 +0900

    개츠비 블로그 스타터 테마 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'development') {
   require("dotenv").config({
-    path: `.env.development`,
+    path: `.env`,
   })
 }
 
@@ -8,20 +8,20 @@ const { NOTION_TOKEN, NOTION_DATABASE } = process.env
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.jae-seong.kim",
-    title: "Jaeseong Kim",
+    title: `김재성의 마이크로에세이 & 개발 블로그`,
+    author: {
+      name: `김재성`,
+      summary: `인문, 사회에 관심이 많은 전산학 전공 대학생`,
+    },
+    description: `김재성의 마이크로에세이 & 개발 블로그`,
+    siteUrl: `https://jae-seong.kim`,
+    social: {
+      github: `sundoufu`,
+      instagram: 'jae.s.kim'
+    },
   },
   plugins: [
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-notion-api`,
       options: {
@@ -31,6 +31,95 @@ module.exports = {
         lowerTitleLevel: true,
       },
     },
-    'gatsby-transformer-remark'
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
   ],
-};
+}
